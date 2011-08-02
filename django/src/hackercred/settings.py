b885757d271708d7ae9882ac295b290200b5333f
Christopher Berner <christopherberner@gmail.com>
2011-08-01 21:16:14 -0700

    Use postgres

--- django/src/hackercred/settings.py
+++ django/src/hackercred/settings.py
@@ -1,4 +1,5 @@
 # Django settings for hackercred project.
+import passwords
 import secretkey
 
 DEBUG = True
@@ -12,10 +13,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/christopher/Code/hackercred/django/hackercred/src/sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'hackercred',                      # Or path to database file if using sqlite3.
+        'USER': 'hackercred',                      # Not used with sqlite3.
+        'PASSWORD': passwords.DATABASE,                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -117,9 +118,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
