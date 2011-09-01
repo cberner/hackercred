f1cc69b16f6166c8415bf0e80dae1ad318d4f211
Christopher Berner <christopherberner@gmail.com>
2011-09-01 00:01:44 -0700

    Start migrating to Bootstrap.js

---
+++ django/src/hackercred/app/static/javascript.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$("body").bind("click", function(e) {
+		$('a.menu').parent("li").removeClass("open");
+	});
+
+	$("a.menu").click(function(e) {
+		var $li = $(this).parent("li").toggleClass('open');
+		return false;
+	});
+});
+
+
+
+
+
+
